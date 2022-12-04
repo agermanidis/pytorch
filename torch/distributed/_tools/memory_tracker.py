9e790cff12cc0c715f0392031cfc3a1e0d84ccc1
Yanli Zhao <yanlizhao@fb.com>
2022-12-04 22:15:06 +0000

    update show traces

--- torch/distributed/_tools/memory_tracker.py
+++ torch/distributed/_tools/memory_tracker.py
@@ -144,7 +144,36 @@ class MemoryTracker:
 
     @no_type_check
     def show_traces(self, path: str = "") -> None:
+        from itertools import chain
+
         import matplotlib.pyplot as plt
+
+        def _plot_figure(x, y_values, labels):
+            min_val = min(list(chain(*y_values))) * 0.999
+            max_val = max(list(chain(*y_values))) * 1.001
+            plt.figure()
+            for y, label in zip(y_values, labels):
+                plt.plot(x, y, label=label)
+            plt.xlabel("# Operator Calls")
+            plt.ylabel("Memory (MB)")
+            plt.legend()
+            for marker_name, marker in self._markers.items():
+                if marker_name == "fw_bw_boundary":
+                    plt.plot(
+                        [marker, marker],
+                        [min_val, max_val],
+                        "r",
+                        lw=2,
+                        label=marker_name,
+                    )
+                else:
+                    plt.plot(
+                        [marker, marker],
+                        [min_val, max_val],
+                        "k-",
+                        lw=2,
+                        label=marker_name,
+                    )
 
         if path != "":
             self.load(path)
@@ -152,41 +181,17 @@ class MemoryTracker:
         y_1 = [gb for (name, gb) in self.memories_allocated.values()]
         y_2 = [gb for (name, gb) in self.memories_active.values()]
         y_3 = [gb for (name, gb) in self.memories_reserved.values()]
-        min_val = min(y_1 + y_2 + y_3) * 0.9
-        max_val = max(y_1 + y_2 + y_3) * 1.1
         x = list(range(len(y_1)))
-        #
-        plt.figure(1)
-        plt.plot(x, list(y_1), label="allocated_memory")
-        plt.plot(x, list(y_2), label="active_memory")
-        plt.plot(x, list(y_3), label="reserved_memory")
-        plt.xlabel("# Operator Calls")
-        plt.ylabel("Memory (MB)")
-        plt.legend()
-        plt.figure(2)
-        plt.plot(x, list(y_1), label="allocated_memory")
-        plt.xlabel("# Operator Calls")
-        plt.ylabel("Memory (MB)")
-        plt.legend()
-        plt.figure(3)
-        plt.plot(x, list(y_2), label="active_memory")
-        plt.xlabel("# Operator Calls")
-        plt.ylabel("Memory (MB)")
-        plt.legend()
-        plt.figure(4)
-        plt.plot(x, list(y_3), label="reserved_memory")
-        plt.xlabel("# Operator Calls")
-        plt.ylabel("Memory (MB)")
-        plt.legend()
-        for marker_name, marker in self._markers.items():
-            if marker_name == "fw_bw_boundary":
-                plt.plot(
-                    [marker, marker], [min_val, max_val], "r", lw=2, label=marker_name
-                )
-            else:
-                plt.plot(
-                    [marker, marker], [min_val, max_val], "k-", lw=2, label=marker_name
-                )
+        # Split figures when there is big difference between 
+        # "reserved_memory" and "allocated_memory" or "active_memory".
+        _plot_figure(
+            x,
+            [list(y_1), list(y_2), list(y_3)],
+            ["allocated_memory", "active_memory", "reserved_memory"],
+        )
+        _plot_figure(x, [list(y_1)], ["allocated_memory"])
+        _plot_figure(x, [list(y_2)], ["active_memory"])
+        _plot_figure(x, [list(y_3)], ["reserved_memory"])
 
     def save_stats(self, path: str) -> None:
         """
@@ -203,7 +208,7 @@ class MemoryTracker:
         }
 
         with open(path, "wb") as f:
-            pickle.dump(stats, f)
+            pickle.dump(stats, f, pickle.HIGHEST_PROTOCOL)
 
     def load(self, path: str) -> None:
         """
